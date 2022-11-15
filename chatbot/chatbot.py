60b0c7e18263adfa97c2368579dd5bdd3f403b45
ChickenBenny <zxc123benny14159@gmail.com>
2022-11-15 15:14:50 +0800

    fix: path error

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -8,7 +8,7 @@ class Chatbot:
 
     FILE = "data.pth"
 
-    with open('intents.json', 'r') as f:
+    with open('./assets/intents.json', 'r') as f:
         intents = json.load(f)
 
     def __init__(self):
@@ -39,12 +39,12 @@ class Chatbot:
             if sentence == "quit":
                 break
 
-            sentence = self.prep._tokenize(sentence)
-            X = self.prep.bag_of_words(sentence, self.all_words)
-            X = X.reshape(1, X.shape[0])
-            X = torch.from_numpy(X).to(self.device)
+            sentence = self.prep.chatbot_msg_process(sentence)
+            x = self.prep.bag_of_words(sentence, self.all_words)
+            x = x.reshape(1, x.shape[0])
+            x = torch.from_numpy(x).to(self.device)
 
-            output = self.model(X)
+            output = self.model(x)
             _, predicted = torch.max(output, dim=1)
 
             tag = self.tags[predicted.item()]
